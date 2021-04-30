52c5c51f6cd2afc1736d7e23f39d8d37e1c8acbb
Oleh <olehfromukraine@gmail.com>
2021-04-30 23:22:40 +0300

    create Security project

---
+++ Security/src/main/java/com/example/security/student/StudentController.java
@@ -0,0 +1,26 @@
+package com.example.security.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "Ricko Brama"),
+            new Student(2, "Michel Richka"),
+            new Student(3, "Nick Stodola")
+    );
+
+    @GetMapping(path="/{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId){
+        return STUDENTS.stream().filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Student " + studentId + " don't exists"));
+    }
+}
